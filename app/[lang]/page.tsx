72f889ed4c589710d7525cd160f6c642a2b8c5e3
ilyaivanov239-rgb <ilyaivanov239@gmail.com>
2025-09-18 13:43:12 +0400

    Update page.tsx

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -126,7 +126,7 @@ export default function Page() {
    { name: 'ERCO', src: '/images/brands/erco.svg', href: 'https://www.erco.com/en/' },
   { name: 'XAL',  src: '/images/brands/xal-white.webp', href: 'https://www.xal.com/en', invert: true },
   { name: 'BEGA', src: '/images/brands/bega.svg', href: 'https://www.bega.com/en/' },
-    { name: 'Luce&Light', src: '/images/brands/luceandlight.svg', href: 'https://www.lucelight.it/en/' },
+    { name: 'Luce&Light', src: '/images/brands/Luceandlight.svg', href: 'https://www.lucelight.it/en/' },
     { name: 'Brand 5', src: '/images/brands/brand5.png' },
     { name: 'Brand 6', src: '/images/brands/brand6.png' },
   ];
