db6ce073aa740c372388ad14c766467226279cbb
ilyaivanov239-rgb <ilyaivanov239@gmail.com>
2025-09-18 13:52:07 +0400

    Update page.tsx

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -123,10 +123,10 @@ export default function Page() {
 
   // Бренды (заглушки)
   const BRANDS: Brand[] = [
-   { name: 'ERCO', src: '/images/brands/erco.svg', href: 'https://www.erco.com/en/' },
-  { name: 'XAL',  src: '/images/brands/xal-white.webp', href: 'https://www.xal.com/en', invert: true },
-  { name: 'BEGA', src: '/images/brands/bega.svg', href: 'https://www.bega.com/en/' },
-    { name: 'Luce&Light', src: '/images/brands/Luceandlight.svg', href: 'https://www.lucelight.it/en/' },
+   { name: 'ERCO', src: '/images/brands/erco.svg', href: 'https://www.erco.com/en/', scale: 1.1  },
+  { name: 'XAL',  src: '/images/brands/xal-white.webp', href: 'https://www.xal.com/en', invert: true, scale: 1.0  },
+  { name: 'BEGA', src: '/images/brands/bega.svg', href: 'https://www.bega.com/en/', scale: 1.1  },
+    { name: 'Luce&Light', src: '/images/brands/Luceandlight.svg', href: 'https://www.lucelight.it/en/', scale: 1.2  },
     { name: 'Brand 5', src: '/images/brands/brand5.png' },
     { name: 'Brand 6', src: '/images/brands/brand6.png' },
   ];
